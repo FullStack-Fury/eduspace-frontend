f3cb11a61dc3f2b001d0321e769c27e742b96c3a
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-22 11:05:51 -0500

    feat: prime vue added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,72 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import i18n from "./i18n.js";
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
-const app = createApp(App)
+import 'primeflex/primeflex.css'
+import ConfirmationService from "primevue/confirmationservice";
+import DialogService from "primevue/dialogservice";
+import ToastService from "primevue/toastservice";
+import Button from "primevue/button";
+import Card from "primevue/card";
+import Column from "primevue/column";
+import ConfirmDialog from "primevue/confirmdialog";
+import Checkbox from "primevue/checkbox";
+import DataTable from "primevue/datatable";
+import Dialog from "primevue/dialog";
+import Select from "primevue/select";
+import SelectButton from "primevue/selectbutton";
+import FileUpload from "primevue/fileupload";
+import FloatLabel from "primevue/floatlabel";
+import IconField from "primevue/iconfield";
+import InputIcon from "primevue/inputicon";
+import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
+import Menu from "primevue/menu";
+import Rating from "primevue/rating";
+import Row from "primevue/row";
+import Drawer from "primevue/drawer";
+import Tag from "primevue/tag";
+import Textarea from "primevue/textarea";
+import Toolbar from "primevue/toolbar";
+import Toast from "primevue/toast";
 
-app.mount('#app')
+app.use(PrimeVue, { theme: { preset:Aura },  ripple: true })
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+
+//primevue components
+
+app.component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-column', Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-data-table', DataTable)
+    .component('pv-dialog', Dialog)
+    .component('pv-select', Select)
+    .component('pv-select-button', SelectButton)
+    .component('pv-file-upload', FileUpload)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-icon-field', IconField)
+    .component('pv-input-icon', InputIcon)
+    .component('pv-input-text', InputText)
+    .component('pv-input-number', InputNumber)
+    .component('pv-menu', Menu)
+    .component('pv-rating', Rating)
+    .component('pv-row', Row)
+    .component('pv-drawer', Drawer)
+    .component('pv-tag', Tag)
+    .component('pv-textarea', Textarea)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-toast', Toast);
+
+const app = createApp(App);
+
+app.use(i18n);
+
+
+app.mount('#app');
