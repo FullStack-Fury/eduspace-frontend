ed02adf1c9402dffc7477d2e7a76ffd178e727a4
petitavo <Gustavohuanca1020@gmail.com>
2024-09-28 02:00:50 -0500

    feat(services): fixed.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import MeetManagementComponent from "../events-scheduling/pages/meet-management.component.vue";
 import ReservationManagementComponent from "../reservation-scheduling/pages/reservation-management.component.vue";
 import PersonnelList from "../teacher-management/components/personnel-list.vue";
+import TeacherInformation from "../teacher-management/components/teacher-information.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,6 +10,7 @@ const router = createRouter({
         { path: '/events-scheduling/meet', name: 'meet', component: MeetManagementComponent, meta: {title: 'Meeting Management'} },
         { path: '/reservation-scheduling', name: 'reservation', component: ReservationManagementComponent, meta: {title: 'Reservation Management'} },
         { path: '/teacher-data', name: 'teacher', component: PersonnelList, meta: {title: 'Personnel List'} },
+        {path: '/add-personnel', name: 'AddPersonnel', component: TeacherInformation, meta: {title: 'Add Personnel'}}
 
     ]
 });
