6d1b7f559415a55dbd8b58240e6eb147f4e82589
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-14 06:19:19 -0500

    feat(index-js): added payroll-management route.

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import MeetManagementComponent from "../events-scheduling/pages/meet-management.
 import ReservationManagementComponent from "../reservation-scheduling/pages/reservation-management.component.vue";
 import HomeComponent from "../public/pages/home.component.vue";
 import LoginComponent from "../public/pages/login.component.vue";
+import PayrollManagementComponent from "../payroll-management/pages/payroll-management.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/events-scheduling/meet', name: 'meet', component: MeetManagementComponent, meta: {title: 'Meeting Management'} },
         { path: '/reservation-scheduling', name: 'reservation', component: ReservationManagementComponent, meta: {title: 'Reservation Management'} },
+        { path: '/payroll-management', name: 'payroll', component: PayrollManagementComponent, meta: {title: 'Payroll Management'} },
         { path: '/home', name: 'home', component: HomeComponent, meta: { title: 'Home' } },
         { path: '/login', name: 'login', component: LoginComponent, meta: { title: 'login' } }
     ]
