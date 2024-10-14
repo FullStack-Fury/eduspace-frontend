aff4cc1f309518befa0eb485e6ec2d0c95528f81
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-14 06:19:59 -0500

    feat(payroll): create and set payroll entity.

---
+++ src/payroll-management/model/payroll.entity.js
@@ -0,0 +1,21 @@
+export class Payroll {
+    constructor({
+                    id = 0,
+                    teacherId = 0,
+                    salaryAmount = 0,
+                    pensionContribution = 0,
+                    salaryBonus = 0,
+                }) {
+        this.id = id;
+        this.teacherId = teacherId;
+        this.salaryAmount = salaryAmount;
+        this.pensionContribution = pensionContribution;
+        this.salaryBonus = salaryBonus;
+        this.salaryNet = this.calculateNetSalary();
+    }
+
+    // Método para calcular el salario neto
+    calculateNetSalary() {
+        return this.salaryAmount + this.salaryBonus - this.pensionContribution;
+    }
+}
