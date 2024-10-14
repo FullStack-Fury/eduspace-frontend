badee12baa8029b121f364e693cd875d4290a9ba
AngeloC12 <angelocuri@hotmail.com>
2024-10-14 11:31:26 -0500

    feat: Routes for new components are added

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import MeetManagementComponent from "../events-scheduling/pages/meet-management.
 import ReservationManagementComponent from "../reservation-scheduling/pages/reservation-management.component.vue";
 import HomeComponent from "../public/pages/home.component.vue";
 import LoginComponent from "../public/pages/login.component.vue";
+import ClassroomManagementComponent from "../breakdown-management/pages/classroom-management.component.vue";
+import ResourceManagementComponent from "../breakdown-management/pages/resource-management.component.vue";
+import ReportResourceComponent from "../breakdown-management/pages/report-resource.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -10,7 +13,10 @@ const router = createRouter({
         { path: '/events-scheduling/meet', name: 'meet', component: MeetManagementComponent, meta: {title: 'Meeting Management'} },
         { path: '/reservation-scheduling', name: 'reservation', component: ReservationManagementComponent, meta: {title: 'Reservation Management'} },
         { path: '/home', name: 'home', component: HomeComponent, meta: { title: 'Home' } },
-        { path: '/login', name: 'login', component: LoginComponent, meta: { title: 'login' } }
+        { path: '/login', name: 'login', component: LoginComponent, meta: { title: 'login' } },
+        { path: '/classrooms', name: 'classrooms', component: ClassroomManagementComponent, meta: {title: 'Classroom Management'} },
+        { path: '/classroom/:classroomId/resources', name: 'resource-management', component: ResourceManagementComponent, meta: { title: 'Resource Management' }, props: true },
+        { path: '/classroom/:classroomId/resources/:resourceId/reports', name: 'report-resource', component: ReportResourceComponent, meta: { title: 'Report Resource' } },
     ]
 });
 
