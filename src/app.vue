7791c3c3867295401fa9f2f4a02e43985287410f
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-15 15:22:27 -0500

    refactor(app-vue): toolbar change for teacher and logout working

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,6 @@
 <script>
 import LanguageSwitcher from "./public/components/language-switcher.component.vue";
-import {mapGetters} from "vuex";
+import {mapActions, mapGetters} from "vuex";
 
 export default {
   name: 'app',
@@ -21,8 +21,15 @@ export default {
     ...mapGetters('user', ['userId', 'userRole'])
   },
   methods: {
+    ...mapActions('user', ['setUser', 'clearUser']),
     toggleDrawer() {
       this.drawer = !this.drawer;
+    },
+    handleLogOut() {
+      this.clearUser();
+      console.log('user cleared', this.userId, this.userRole)
+
+      this.$router.push({ name: 'login' });
     },
     changeToolbar() {
       if(this.userRole === 1) {
@@ -30,16 +37,22 @@ export default {
         this.items = [
           { label: 'Home', to: '/home' },
           { label: 'Meet', to: '/events-scheduling/meet' },
-          { label: 'Reservation', to: '/reservation-scheduling'},
           { label: 'Salary Calculation', to: '/salary-calculation'},
-          { label: 'Log out', to: '/login'},
-          { label: 'Classrooms', to: '/classrooms'},
+          { label: 'Log out', onClick: this.handleLogOut },
         ]
 
       }
-      else {
-        console.log('welcome my bro')
+      else if(this.userRole === 2) {
+        console.log('welcome teacher')
 
+        this.items = [
+          { label: 'Home', to: '/home' },
+          { label: 'Reservation', to: '/reservation-scheduling'},
+          { label: 'Classrooms', to: '/classrooms'},
+          { label: 'Log out', onClick: this.handleLogOut },
+        ]
+      }
+      else {
         this.items = [
           { label: 'Home', to: '/home' },
           { label: 'Login', to: '/login' }
@@ -67,10 +80,21 @@ export default {
         <h2>EduSpace</h2>
       </template>
       <template #center>
-        <div class="flex-column">
-          <router-link v-for="item in items" :key="item.label" v-slot="{navigate, href}" :to="item.to" custom>
-            <pv-button :href="href" class="p-button-text" @click="navigate">{{ item.label }}</pv-button>
-          </router-link>
+        <div class=" flex flex-row">
+          <div v-for="item in items" :key="item.label">
+            <router-link v-if="item.to"
+                         :to="item.to"
+            >
+              <pv-button class="p-button-text">
+                {{ item.label}}
+              </pv-button>
+            </router-link>
+            <pv-button v-else
+                       @click="item.onClick">
+              {{ item.label }}
+            </pv-button>
+          </div>
+
         </div>
       </template>
       <template #end>
