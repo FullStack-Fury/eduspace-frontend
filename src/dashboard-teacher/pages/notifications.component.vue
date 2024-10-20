5f47a44a38a0e151d320b1e4a2bcd1e49c1322aa
C7leo <149843920+C7leo@users.noreply.github.com>
2024-10-20 01:14:24 -0500

    feat(dashboard-teacher): added components

---
+++ src/dashboard-teacher/pages/notifications.component.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "notifications"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/dashboard-teacher/pages/space-availability.component.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "space-availability"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
