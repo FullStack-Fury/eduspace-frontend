4613d28249887e70274e6b3660d83e377bf01965
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-22 19:54:16 -0500

    feat(index-js): updated.

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,22 @@ import ReservationManagementComponent from '../reservation-scheduling/pages/rese
 import HomeComponent from '../public/pages/home.component.vue';
 import LoginComponent from '../public/pages/login.component.vue';
 import PayrollManagementComponent from '../payroll-management/pages/payroll-management.component.vue';
-import PayrollCreateAndEdit from '../payroll-management/pages/payroll-create-wrapper.component.vue'; // Asegúrate de importar correctamente
+import PayrollCreateAndEdit from '../payroll-management/pages/payroll-create-wrapper.component.vue';
 import ClassroomManagementComponent from "../breakdown-management/pages/classroom-management.component.vue";
 import ResourceManagementComponent from "../breakdown-management/pages/resource-management.component.vue";
 import ReportResourceComponent from "../breakdown-management/pages/report-resource.component.vue";
+import PayrollSummaryComponent from '../payroll-management/pages/payroll-summary.component.vue'; // Nueva importación
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/events-scheduling/meet', name: 'meet', component: MeetManagementComponent, meta: {title: 'Meeting Management'} },
         { path: '/reservation-scheduling', name: 'reservation', component: ReservationManagementComponent, meta: {title: 'Reservation Management'} },
-        { path: '/salary-calculation', name: 'salary-calculation', component: PayrollManagementComponent, meta: { title: 'Payroll Management' } },
-        { path: '/salary-calculation/create', name: 'create-payroll', component: PayrollCreateAndEdit, meta: { title: 'Create Payroll' } }, // Nueva ruta para crear payroll
+        { path: '/salary-calculation', name: 'salary-calculation', component: PayrollSummaryComponent, meta: { title: 'Salary Summary' } }, // Ruta al Summary
+        { path: '/salary-calculation/management', name: 'payroll-management', component: PayrollManagementComponent, meta: { title: 'Payroll Management' } }, // Ruta a Payroll Management
+        { path: '/salary-calculation/create', name: 'create-payroll', component: PayrollCreateAndEdit, meta: { title: 'Create Payroll' } },
         { path: '/home', name: 'home', component: HomeComponent, meta: { title: 'Home' } },
-        { path: '/login', name: 'login', component: LoginComponent, meta: { title: 'login' } },
+        { path: '/login', name: 'login', component: LoginComponent, meta: { title: 'Login' } },
         { path: '/classrooms', name: 'classrooms', component: ClassroomManagementComponent, meta: {title: 'Classroom Management'} },
         { path: '/classroom/:classroomId/resources', name: 'resource-management', component: ResourceManagementComponent, meta: { title: 'Resource Management' }, props: true },
         { path: '/classroom/:classroomId/resources/:resourceId/reports', name: 'report-resource', component: ReportResourceComponent, meta: { title: 'Report Resource' } },
@@ -29,4 +31,5 @@ router.beforeEach((to, from, next) => {
     document.title = `${baseTitle} | ${to.meta['title']}`;
     next();
 })
+
 export default router;
