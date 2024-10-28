ac8f1465774a44b9348c37dc37ac993ab276151b
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-27 20:24:52 -0500

    feat(payroll): fixed payroll entity.

--- src/payroll-management/model/payroll.entity.js
+++ src/payroll-management/model/payroll.entity.js
@@ -5,16 +5,31 @@ export class Payroll {
                     salaryAmount = 0,
                     pensionContribution = 0,
                     salaryBonus = 0,
+                    paymentMethod = "",
+                    account = "",
+                    observation = "",
+                    datePayment = null,
+                    otherDeductions = 0,
+                    serie = "A",  // Campo Serie Automática
                 }) {
         this.id = id;
         this.teacherId = teacherId;
         this.salaryAmount = salaryAmount;
         this.pensionContribution = pensionContribution;
         this.salaryBonus = salaryBonus;
+        this.otherDeductions = otherDeductions;
+        this.paymentMethod = paymentMethod;
+        this.account = account;
+        this.observation = observation;
+        this.datePayment = datePayment;
+        this.serie = serie; // Guardamos la serie automática
+
+        // Calculamos el salario neto al crear la instancia
         this.salaryNet = this.calculateNetSalary();
     }
 
     calculateNetSalary() {
-        return this.salaryAmount + this.salaryBonus - this.pensionContribution;
+        const totalDeductions = this.pensionContribution + this.otherDeductions;
+        return this.salaryAmount + this.salaryBonus - totalDeductions;
     }
 }
