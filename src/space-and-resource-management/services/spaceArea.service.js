ad063a42ed8d9387f015a469590e1f0e6cf73ad6
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-29 18:13:18 -0500

    refactor: change name space area service

--- src/space-and-resource-management/services/spaceArea.service.js
+++ src/space-and-resource-management/services/spaceArea.service.js
@@ -1,6 +1,6 @@
 import http from "../../shared/services/http-common.js";
 
-export class SpaceReservationService {
+export class SpaceAreaService {
     spaceAreaEndPoint = '/shared-area'
 
     getAll() {
