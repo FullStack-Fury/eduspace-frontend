1cd24f11e5519ae18e3b61dc88ab3b3a574b75f9
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-29 15:59:39 -0500

    feat: create the service http for the space area

---
+++ src/space-and-resource-management/services/spaceArea.service.js
@@ -0,0 +1,21 @@
+import http from "../../shared/services/http-common.js";
+
+export class SpaceReservationService {
+    spaceAreaEndPoint = '/shared-area'
+
+    getAll() {
+        return http.get(this.spaceAreaEndPoint);
+    }
+
+    create(space) {
+        return http.post(this.spaceAreaEndPoint, space);
+    }
+
+    delete(id) {
+        return http.delete(`${this.spaceAreaEndPoint}/${id}`);
+    }
+
+    findAllByAreaId(areaId) {
+        return http.get(`${this.spaceAreaEndPoint}?areaId=${areaId}`)
+    }
+}
