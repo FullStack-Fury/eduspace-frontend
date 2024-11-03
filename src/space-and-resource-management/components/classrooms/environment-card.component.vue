36215339040bf2c45585548e90beec4e337ff1ef
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-11-03 03:03:11 -0500

    feat: added environment card

---
+++ src/space-and-resource-management/components/classrooms/environment-card.component.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: "environment-card",
+  props: {
+    environment: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    async deleteEnvironment() {
+      try {
+        await this.$emit('delete', this.environment.id);
+      } catch (error) {
+        console.error("Error deleting environment:", error);
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="card border-round-xl shadow-2 p-3 h-full">
+      <div class="text-xl font-semibold mb-2">{{ environment.name }}</div>
+      <div class="mb-2">
+        <span class="font-medium text-600">Description: {{ environment.description }}</span>
+      </div>
+      <div class="flex align-items-center gap-2 mb-3">
+        <i class="pi pi-box text-yellow-500"></i>
+        <span>Environment</span>
+      </div>
+      <pv-button label="Delete" severity="danger" text raised class="w-full" @click="deleteEnvironment"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  align-content: space-between;
+}
+
+.card {
+  width: 300px;
+}
+</style>
