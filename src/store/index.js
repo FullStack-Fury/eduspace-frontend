7addf1ed2995b2de176e08020e573c0bc68d52ec
Oscar Antayhua <oscarjac2001@gmail.com>
2024-11-03 00:03:15 -0500

    feat(index-js): vuex updated

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,32 @@
-import { createStore }from 'vuex'
-import user from './modules/user.js'
+import { createStore } from 'vuex';
 
 export default createStore({
-    modules: {
-        user,
+    state: {
+        userId: null,
+        userRole: null // El rol del usuario (1 = Admin, 2 = Teacher)
     },
-})
+    mutations: {
+        setUser(state, userData) {
+            state.userId = userData.id;
+            state.userRole = userData.role;
+        },
+        clearUser(state) {
+            state.userId = null;
+            state.userRole = null;
+        }
+    },
+    actions: {
+        setUser({ commit }, userData) {
+            localStorage.setItem('userRole', userData.role); // Guarda el rol en localStorage
+            commit('setUser', userData);
+        },
+        clearUser({ commit }) {
+            localStorage.removeItem('userRole'); // Elimina el rol de localStorage
+            commit('clearUser');
+        }
+    },
+    getters: {
+        userId: (state) => state.userId,
+        userRole: (state) => state.userRole
+    }
+});
